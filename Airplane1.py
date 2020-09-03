6a1cbbdef6be12b351fa04a4340503bd0afc59da
Vlad Pavlovich Kruglov <67640621+Vladkruglov@users.noreply.github.com>
2020-09-03 20:47:03 +0300

    Update Airplane1.py

--- Airplane1.py
+++ Airplane1.py
@@ -9,26 +9,27 @@ companies = ["S7", "British Airlines", "Delta", "Уральские Авиали
 class Airplane(object):
 
     
-    def __init__(self, x, y, company, speed, color):
+    def __init__(self, x, y, company, speed, color, bearing):
         self.company = company
         self.x = x
         self.y = y
         self.speed = speed
         self.color = color
+        self.bearing = bearing
 
-    def __init__(self, max_height = 700, max_width = 700, max_speed = 1000):
+    def __init__(self, max_height = 700, max_width = 700, max_speed = 1000, max_bearing = 360):
         self.x = random.randint(0, max_height)
         self.y = random.randint(0, max_width)
         self.speed = random.randint(0, max_speed)
         self.color = random.choice(color)
         self.company = random.choice(companies)
+        self.bearing = random.randint(0, max_bearing)
 
     def __str__(self):
         return "Самолёт компании {}, в координатах {}, {}, со скоростью {}, имеет цвет {}.".format(self.company, self.x, self.y, self.speed, self.color)
 
     def draw(self, c):
         c.create_oval(self.x - 5, self.y - 5, self.x + 5, self.y + 5, fill = self.color) 
-        c.txt(self.speed)  
 
 
 
@@ -61,24 +62,38 @@ if __name__ == "__main__":
 
     t = Tk()
     t.title("Airplanes                                                                 0.6.4")  
-    canvas = Canvas(t, height = 700, width = 700)
+    canvas = Canvas(t,height = 700,width = 700)
     canvas.pack()
     canvas.create_oval(-1000,-1000,1000,1000, fill = 'yellowgreen')
-    canvas.create_line(70, 0, 70, 700)
-    canvas.create_line(140, 0, 140, 700)
-    canvas.create_line(210, 0, 210, 700)
-    canvas.create_line(280, 0, 280, 700)
-    canvas.create_line(350, 0, 350, 700)
-    canvas.create_line(420, 0, 420, 700)
-    canvas.create_line(490, 0, 490, 700)
-    canvas.create_line(560, 0, 560, 700)
-    canvas.create_line(630, 0, 630, 700)
+    r = 0
+    z = 70
+    while r < 10:
+        r = r + 1
+        canvas.create_line(z, 0, z, 700)
+        z = z + 70
+    r = 0
+    z = 70
+    while r < 10:
+        r = r + 1
+        canvas.create_line(0, z, 700, z)
+        z = z + 70
+    canvas.create_line(0, 70, 700, 70)
+    canvas.create_line(0, 140, 700, 140)
+    canvas.create_line(0, 210, 700, 210)
+    canvas.create_line(0, 280, 700, 280)
+    canvas.create_line(0, 350, 700, 350)
+    canvas.create_line(0, 420, 700, 420)
+    canvas.create_line(0, 490, 700, 490)
+    canvas.create_line(0, 560, 700, 560)
+    canvas.create_line(0, 630, 700, 630)
+
     for i in range(0, 20):
         airplane = Airplane()
         airplanes.append(airplane)
     
-    for plane in airplanes:
-        plane.draw(canvas)
+    for airplane in airplanes:
+        airplane.draw(canvas)
+    t.mainloop()
 
 
 
